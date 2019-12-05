5d22a263ea52fee9a278d4b7440548fc3d4d792d
Raphael Felipe <raphaelfelipe999@gmail.com>
2019-12-05 18:24:57 -0300

    adding project

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	 float timeVel, constVel;
+
+//========================================================
+	WalkDetector device;
+//=======================================================
+
+	private Rigidbody rb;
+
+	public GameObject cam;
+	public GameObject CamPos;
+	public GameObject CamPrefab;
+
+
+	void  Start (){
+
+		rb = GetComponent<Rigidbody>();
+	
+		cam = Instantiate(CamPrefab) as GameObject;
+		cam.GetComponent<CamControl>().cameraPosition = CamPos;
+
+		device = new WalkDetector();
+	}		
+
+	void Update (){
+
+		PlayerDirection();
+		PlayerPosition();
+		ConstantVelocity();
+
+	}
+
+	bool InGround(){
+		return Physics.Raycast (
+			transform.position, //ponto inicial
+			-transform.up,//direção do raio
+			transform.GetComponent<CapsuleCollider>().height/3//comprimento
+		);
+	}
+		
+	void ConstantVelocity(){
+		if(device.IsWalking())
+			timeVel += 0.9f * Time.deltaTime;
+		else
+			timeVel -= 1f * Time.deltaTime;
+
+		if(timeVel <= 0)
+			constVel = timeVel = 0;
+
+		if(timeVel > 1f)
+			timeVel = constVel = 1f;
+		else
+			constVel = timeVel*0.85f;
+	}
+
+	void PlayerDirection(){
+		Vector3 rot = cam.transform.eulerAngles;
+		rot.x = rot.z = 0;
+		transform.eulerAngles = rot;
+	}
+
+	void PlayerPosition(){
+		
+		if(device.OnMove()){
+			if(InGround()){
+				rb.velocity = transform.forward * ( 5 * constVel);
+			}else{
+				rb.MovePosition(transform.position + transform.forward * (5 * 0.5f * constVel) * Time.deltaTime);
+			}
+		}else{
+			rb.MovePosition(transform.position);
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/WalkDetector.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+
+public class WalkDetector : MonoBehaviour {
+
+	// Use this for initialization
+
+	//========================================================
+	Vector3 Diferences, CurrentValues, PastValues = new Vector3(9.9f,9.9f,9.9f), lowPassValues;
+	Vector3 currentMean, pastMean, meanDif;
+	float minYpass, maxYpass, middleYpass, zpass, xpass;//for xioami values
+
+	public bool[] validWalk = new bool[3];
+	bool needRevalidation;
+
+	float accelerometerUpdateInterval;
+	float lowPassKernelWidthInSeconds;
+	float lowPassFilterFactor;
+
+	//=======================================================
+
+	public void Start (){
+
+		lowPassValues = Input.acceleration;
+
+		accelerometerUpdateInterval = 1.0f / 60.0f;
+		lowPassKernelWidthInSeconds = 1.0f;
+		lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+		
+		GetDiviceType();
+	}		
+
+	float LowPassFilterAccelerometer (float LowPassValue, float AxisAcceleration){
+		// Smooths out noise from accelerometer data
+		LowPassValue = Mathf.Lerp(LowPassValue, AxisAcceleration, lowPassFilterFactor);
+		LowPassValue = Mathf.Sqrt(LowPassValue*LowPassValue);
+		return LowPassValue;
+	}
+
+	float GetDiferenceAbsolute(float a, float b){
+		return Mathf.Abs(a-b);
+	}
+
+	public bool IsWalking(){
+		// - it's recommended don't change this values -
+
+		//checking if the device's move it's a true walk or invalid shake
+		if(
+			validWalk[1] 
+			&& validWalk[2]
+			&& validWalk[0]
+		)
+			return true;
+		else
+			return false;
+		
+	}
+
+	void MeanOfValues(){
+		currentMean = Vector3.zero;
+		for(int i = 0; i < 3; i++){
+			currentMean[i] = (CurrentValues[i] + pastMean[i])/2;
+			meanDif[i] = GetDiferenceAbsolute(pastMean[i],currentMean[i]);
+			pastMean[i] = Mathf.Abs(currentMean[i]);
+		}
+	}
+
+	public void ValidStepWalk(){
+		//it's recommended don't change this values.
+
+
+		//Check if player continues in moving (on axis Y)
+		if(
+			meanDif[1] >  minYpass &&
+			meanDif[1] <= maxYpass
+		){
+			//Check if the first step is valid (on axis Y)
+			if(
+				meanDif[1] >  middleYpass &&
+				!needRevalidation
+			){
+				validWalk[1] = true;
+			}else{
+				needRevalidation = false;
+			}
+		}else{
+			validWalk[1] = false;
+			needRevalidation = true;
+		}
+
+
+		//Check if the first step is valid (on axis Z)
+		if(
+			meanDif[2] >  zpass
+		){
+			validWalk[2] = true;
+		}else
+			validWalk[2] = false;
+		
+		//Check if device is not shaking (on axis x)
+		if(
+			meanDif[0] <  xpass
+		){
+			validWalk[0] = true;
+		}else
+			validWalk[0] = false;
+		
+	}
+
+	public bool OnMove (){
+		
+		CurrentValues.x = LowPassFilterAccelerometer(lowPassValues.x,Input.acceleration.x);
+		CurrentValues.y = LowPassFilterAccelerometer(lowPassValues.y,Input.acceleration.y);
+		CurrentValues.z = LowPassFilterAccelerometer(lowPassValues.z,Input.acceleration.z);
+
+		Diferences.x = GetDiferenceAbsolute(CurrentValues.x, PastValues.x);
+		Diferences.y = GetDiferenceAbsolute(CurrentValues.y, PastValues.y);
+		Diferences.z = GetDiferenceAbsolute(CurrentValues.z, PastValues.z);
+
+		MeanOfValues();
+		ValidStepWalk();
+
+		// Maps the player's real-world steps into in-game movement
+		if(IsWalking()){
+			PastValues.x = Mathf.Abs(CurrentValues.x);
+			PastValues.y = Mathf.Abs(CurrentValues.y);
+			PastValues.z = Mathf.Abs(CurrentValues.z);
+			return true;
+		}else{
+			PastValues.x = Mathf.Abs(CurrentValues.x);
+			PastValues.y = Mathf.Abs(CurrentValues.y);
+			PastValues.z = Mathf.Abs(CurrentValues.z);
+			return false;
+		}
+	}
+	
+	void GetDiviceType(){
+		string device = SystemInfo.deviceModel;
+		string letters = (device[0].ToString() + device[1].ToString()).ToLower();
+		//Is Device xiaomi?
+		if(letters.Equals("mi")){
+			minYpass = 0.00005f;
+			maxYpass = 0.0015f;
+			middleYpass = 0.0002f;
+			zpass = 0.000125f;
+			xpass = 0.005f;
+		}else{
+			//if another device (best values!)
+			minYpass = 0.0001f;
+			maxYpass = 0.0015f;
+			middleYpass = 0.00025f;
+			zpass = 0.00025f;
+			xpass = 0.0045f;
+		}
+	}
+	
+}
